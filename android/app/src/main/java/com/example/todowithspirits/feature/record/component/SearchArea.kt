f561b5e1a93417d33b047a3f611e5f3a68e151ba
junsu1023 <jangdokg12@naver.com>
2026-06-22 21:09:12 +0900

    feat(android): 검색 영역 구현

---
+++ android/app/src/main/java/com/example/todowithspirits/feature/record/component/SearchArea.kt
@@ -0,0 +1,67 @@
+package com.example.todowithspirits.feature.record.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.todowithspirits.R
+import com.example.todowithspirits.theme.SplitsTodoTheme
+
+@Composable
+fun SearchArea(selected: String) {
+    var searchText by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+    ) {
+        BasicTextField(
+            value = searchText,
+            onValueChange = { searchText = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(32.dp)
+                .padding(horizontal = 2.dp),
+            textStyle = TextStyle(
+                color = SplitsTodoTheme.colors.textColor2,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Medium
+            ),
+            singleLine = true,
+            decorationBox = { innerTextField ->
+                if (searchText.isEmpty()) {
+                    Text(
+                        text = stringResource(R.string.search_todo, selected),
+                        style = TextStyle(
+                            color = SplitsTodoTheme.colors.textColor2,
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Medium
+                        )
+                    )
+                }
+
+                innerTextField()
+            }
+        )
+
+        HorizontalDivider(
+            thickness = 1.dp,
+            color = SplitsTodoTheme.colors.dividerColor
+        )
+    }
+}
