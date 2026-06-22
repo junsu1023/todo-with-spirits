26ddf9c5f60ed245ff8cd283cb67aeba998bb8bd
junsu1023 <jangdokg12@naver.com>
2026-06-22 21:08:48 +0900

    feat(android): 공통 헤더 영역 구현

--- android/app/src/main/java/com/example/todowithspirits/theme/Color.kt
+++ android/app/src/main/java/com/example/todowithspirits/theme/Color.kt
@@ -8,26 +8,33 @@ import androidx.compose.ui.graphics.Color
 val splitsTodoColor = SplitsTodoColor(
     selectedColor = Color(0xFF374957),
     unselectedColor = Color(0xFFD1D1D6),
-    textColor1 = Color(0xFFB286FD),
+    titleTextColor = Color(0xFF000000),
+    textColor1 = Color(0xFF857563),
     textColor2 = Color(0xFF8F8170),
     bgColor1 = Color(0xFFFAFAFA),
     bgColor2 = Color(0xFFFFFFFF),
+    dividerColor = Color(0xFFD1D1D6),
     transparent = Color(0x00000000)
 )
 
 class SplitsTodoColor(
     selectedColor: Color,
     unselectedColor: Color,
+    titleTextColor: Color,
     textColor1: Color,
     textColor2: Color,
     bgColor1: Color,
     bgColor2: Color,
+    dividerColor: Color,
     transparent: Color
 ) {
     var selectedColor by mutableStateOf(selectedColor)
         private set
 
     var unselectedColor by mutableStateOf(unselectedColor)
+        private set
+
+    var titleTextColor by mutableStateOf(titleTextColor)
         private set
 
     var textColor1 by mutableStateOf(textColor1)
@@ -40,6 +47,9 @@ class SplitsTodoColor(
         private set
 
     var bgColor2 by mutableStateOf(bgColor2)
+        private set
+
+    var dividerColor by mutableStateOf(dividerColor)
         private set
 
     var transparent by mutableStateOf(transparent)
