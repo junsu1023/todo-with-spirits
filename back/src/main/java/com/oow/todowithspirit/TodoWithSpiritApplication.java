b9defb68c6a043b3f97d0fc296dcdd8253819f4e
KSAhh <66674138+KSAhh@users.noreply.github.com>
2026-06-28 19:53:35 +0900

    chore(back): Disable default security user configuration

--- back/src/main/java/com/oow/todowithspirit/TodoWithSpiritApplication.java
+++ back/src/main/java/com/oow/todowithspirit/TodoWithSpiritApplication.java
@@ -2,10 +2,11 @@ package com.oow.todowithspirit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class TodoWithSpiritApplication {
 
     public static void main(String[] args) {
