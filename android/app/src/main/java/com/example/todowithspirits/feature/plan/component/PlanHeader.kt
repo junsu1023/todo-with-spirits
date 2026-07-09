b27e352aedb75dda756db50b7cda1685ea98b039
junsu1023 <jangdokg12@naver.com>
2026-07-09 20:27:07 +0900

    refactor(android): 수정된 GUI 맞춰 Plan 화면 헤더 Back 제거

--- android/app/src/main/java/com/example/todowithspirits/feature/plan/component/PlanHeader.kt
+++ android/app/src/main/java/com/example/todowithspirits/feature/plan/component/PlanHeader.kt
@@ -1,6 +1,5 @@
 package com.example.todowithspirits.feature.plan.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -8,11 +7,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.todowithspirits.R
 import com.example.todowithspirits.theme.SpiritTodoTheme
 
 @Composable
@@ -23,12 +20,6 @@ fun PlanHeader() {
             .padding(vertical = 12.dp)
             .padding(start = 10.dp, end = 8.dp)
     ) {
-        Image(
-            painter = painterResource(R.drawable.fi_rr_back),
-            contentDescription = null,
-            modifier = Modifier.align(Alignment.CenterStart)
-        )
-
         Text(
             text = "전체 플랜",
             modifier = Modifier.align(Alignment.Center),
