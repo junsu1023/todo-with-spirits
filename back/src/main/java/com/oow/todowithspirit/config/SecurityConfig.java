4bb12ed23b90c6c9277549c2911c7b98f522b950
KSAhh <66674138+KSAhh@users.noreply.github.com>
2026-07-19 20:32:03 +0900

    feat(back): CORS configuration

--- back/src/main/java/com/oow/todowithspirit/config/SecurityConfig.java
+++ back/src/main/java/com/oow/todowithspirit/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
