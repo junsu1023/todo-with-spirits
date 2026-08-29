0eb78d883a55cd973c1893e59f4730b300660412
junsu1023 <jangdokg12@naver.com>
2026-08-29 19:17:25 +0900

    refactor(android): System Splash 적용

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -88,6 +88,9 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.hilt.navigation.compose)
 
+    // Splash
+    implementation(libs.androidx.core.splashscreen)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.runtime.compose)
